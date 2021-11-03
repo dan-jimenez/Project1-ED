8dcfc4d9652b35011afca4c9983bc73bd77dbd80
Danny Jimenez <60985321+dan-jimenez@users.noreply.github.com>
2021-11-03 17:48:01 -0600

    Casi

---
+++ cmake-build-debug/CMakeFiles/Proyecto1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Proyecto1.dir/main.cpp.obj"
+  "Proyecto1.exe"
+  "Proyecto1.exe.manifest"
+  "Proyecto1.pdb"
+  "libProyecto1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Proyecto1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
